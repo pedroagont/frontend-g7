09e9312f545c6302684c9f471df902540f1e3c3f
Pedro A. González <pgonzalez@post.com>
2021-08-24 22:05:45 -0500

    Crear component UpdateProfile y renderizar en App como ruta privada

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Login from './components/Login';
 import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 import ForgotPassword from './components/ForgotPassword';
+import UpdateProfile from './components/UpdateProfile';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -13,6 +14,7 @@ function App() {
       <Router>
         <Switch>
           <PrivateRoute exact path="/" component={ Dashboard }/>
+          <PrivateRoute exact path="/update-profile" component={ UpdateProfile }/>
           <Route path="/signup" component={ Signup } />
           <Route path="/login" component={ Login } />
           <Route path="/forgot-password" component={ ForgotPassword } />
---
+++ src/components/UpdateProfile.js
@@ -0,0 +1,81 @@
+import { useRef, useState } from 'react';
+import { Alert, Button, Card, Form } from 'react-bootstrap';
+import { useAuth } from '../contexts/authContext';
+import { Link } from 'react-router-dom';
+
+function UpdateProfile() {
+  const newEmailRef = useRef();
+  const newPasswordRef = useRef();
+  const confirmNewPasswordRef = useRef();
+  const { currentUser, updateEmail, updatePassword } = useAuth();
+  const [ error, setError ] = useState('');
+  const [ message, setMessage ] = useState('');
+  const [ loading, setLoading ] = useState('');
+
+  async function handleSubmit(e){
+    e.preventDefault();
+
+    if(newEmailRef.current.value === '' && newPasswordRef.current.value === '') {
+      return setError('Nada que cambiar!');
+    }
+
+    if(newPasswordRef.current.value !== confirmNewPasswordRef.current.value) {
+      return setError('Passwords no coinciden!');
+    }
+
+    const promises = [];
+    setError('')
+    setLoading(true)
+
+    if(newEmailRef.current.value !== '' && newEmailRef.current.value !== currentUser.email) {
+      promises.push(updateEmail(newEmailRef.current.value));
+    }
+
+    if(newPasswordRef.current.value !== '') {
+      promises.push(updatePassword(newPasswordRef.current.value));
+    }
+
+    Promise.all(promises)
+      .then(() => setMessage('Credenciales actualizadas!'))
+      .catch(e => setError('Error al actualizar perfil: ' + e))
+      .finally(() => setLoading(false));
+
+
+
+  }
+
+  return (
+    <Card className="w-75 mx-auto mt-5">
+      <Card.Body>
+        <h1 className="display-4 text-center my-3">Actualizar Perfil</h1>
+        { error && error !== '' && <Alert variant="danger">{ error }</Alert> }
+        { message && message !== '' && <Alert variant="success">{ message }</Alert> }
+        <Form onSubmit={ handleSubmit }>
+          <Form.Group className="mb-3" controlId="formEmail">
+            <Form.Label>New email address</Form.Label>
+            <Form.Control ref={ newEmailRef } type="email" placeholder="Dejar en blanco para no actualizar" autoComplete="off" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formPassword">
+            <Form.Label>New password</Form.Label>
+            <Form.Control ref={ newPasswordRef } type="password" placeholder="Dejar en blanco para no actualizar" autoComplete="off" />
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formConfirmPassword">
+            <Form.Label>Confirm new password</Form.Label>
+            <Form.Control ref={ confirmNewPasswordRef } type="password" placeholder="Confirm password" autoComplete="off" />
+          </Form.Group>
+
+          <Button className="w-100" variant="primary" type="submit" disabled={ loading }>
+            Actualizar perfil
+          </Button>
+        </Form>
+        <Card.Text className="text-muted text-center my-3">
+          <Link to="/">Regresar</Link>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default UpdateProfile;
