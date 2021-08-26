45073763051ea2af39000250a82b23fc255c842c
Pedro A. González <pgonzalez@post.com>
2021-08-26 02:37:47 -0500

    Agregar ruta privada /profile para ver componente Profile

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Dashboard from './components/Dashboard';
 import PrivateRoute from './components/PrivateRoute';
 import ForgotPassword from './components/ForgotPassword';
 import UpdateProfile from './components/UpdateProfile';
+import Profile from './components/Profile';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
       <Router>
         <Switch>
           <PrivateRoute exact path="/" component={ Dashboard }/>
+          <PrivateRoute exact path="/profile" component={ Profile }/>
           <PrivateRoute exact path="/update-profile" component={ UpdateProfile }/>
           <Route path="/signup" component={ Signup } />
           <Route path="/login" component={ Login } />
