8cc82f024c32ed9a2869cea5fa75d3030cdb06d8
Pedro A. González <pgonzalez@post.com>
2021-08-30 21:53:56 -0500

    Remover component PostForm y agregar PostList, quitar título

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,11 @@
 import NavigationBar from './NavigationBar';
-import PostForm from './PostForm';
+import PostList from './PostList';
 
 function Dashboard() {
   return (
     <>
       <NavigationBar />
-      <h2 className="display-4 text-center my-5">Hola desde el dashboard! 🕹</h2>
-      <PostForm />
+      <PostList />
     </>
   )
 }
